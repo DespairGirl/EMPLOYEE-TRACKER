2a0ee5a7231d12dcf2710f6610ffd7925d6506ed
MiMi T <truttling.mariah@gmail.com>
2021-03-03 22:01:15 -0500

    update employee

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
-const console_table= require('console.table');
+const console_table = require('console.table');
 const { listenerCount } = require('events');
+const { allowedNodeEnvironmentFlags } = require('process');
 
 
 //Add, Update, Remove employee
@@ -10,50 +11,50 @@ const { listenerCount } = require('events');
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
-  
-    user: 'root',
-     password: 'mimi2131!!',
-    database: 'employee_db',
-  });
 
-  connection.connect((err) => {
+    user: 'root',
+    password: 'mimi2131!!',
+    database: 'employee_db',
+});
+
+connection.connect((err) => {
     if (err) throw err;
     console.log(`connected as id ${connection.threadId}\n`);
     start();
-  });
+});
 
 
-const start =() =>{
+const start = () => {
 
     inquirer
 
-    .prompt([
-        {
-            type: 'list',
-            name: 'mainMenu',
-            message: 'What Would You Like To Do???',
-            choices: ['Add Employee', 'Remove Employee', 'Update Employee','View All Employees', 'View Employee By Department','View Employee By Manager','Edit Employee Role', 'Edit Employee Manager', 'EXIT']
+        .prompt([
+            {
+                type: 'list',
+                name: 'mainMenu',
+                message: 'What Would You Like To Do???',
+                choices: ['Add Employee', 'Remove Employee', 'Update Employee', 'View All Employees', 'View Employee By Department', 'View Employee By Manager', 'Edit Employee Role', 'Edit Employee Manager', 'EXIT']
 
 
-        }
-    ])
+            }
+        ])
 
-        .then((data)=>{
+        .then((data) => {
             switch (data.mainMenu) {
                 case 'Add Employee':
                     addEmployee();
-                    
+
                     break;
-                
+
                 case 'Remove Employee':
                     removeEmployee();
 
                     break;
-               
-                case 'Update Employee':
-                     updateEmployee();
 
-                     break;
+                case 'Update Employee':
+                    updateEmployee();
+
+                    break;
 
                 case 'View All Employees':
                     allEmployee();
@@ -81,87 +82,157 @@ const start =() =>{
                 case 'EXIT':
                     console.log('Goodbye')
 
-                
+
                 default:
                     break;
             }
 
         })
-    
+
 
 };
 
-const pickManager = () =>{
-    const managerArray=[];
-    connection.query('SELECT First_Name, Last_Name FROM employee WHERE Manager_ID is NULL',(err,res)=>{
+const pickManager = () => {
+    const managerArray = [];
+    connection.query('SELECT First_Name, Last_Name FROM employee WHERE Manager_ID is NULL', (err, res) => {
         if (err) throw err;
-        for (let i = 0; i <res.length; i++) {
-          managerArray.push(res[i].First_Name)
-            
+        for (let i = 0; i < res.length; i++) {
+            managerArray.push(res[i].First_Name)
+
         }
     })
 
     return managerArray;
 };
 
-const addRole = () =>{
-    const roleArray=[];
-    connection.query('SELECT *FROM roletype', (err,res)=>{
-        if(err) throw err;
+const addRole = () => {
+    const roleArray = [];
+    connection.query('SELECT *FROM roletype', (err, res) => {
+        if (err) throw err;
         for (let i = 0; i < res.length; i++) {
-             roleAarray.push(res[i].Title);
-            
+            roleAarray.push(res[i].Title);
+
         }
-     })
-
-     return roleArray;
-};
-
-const addEmployee = () =>{
-    inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'firstName',
-            message: 'Enter Empolyee First Name'
-        },
-        {
-            type: 'input',
-            name: 'lastName',
-            message: 'Enter Empolyee Last Name'
-        },
-        {
-            type: 'list',
-            name: 'managerName',
-            message: 'Enter Empolyee Manager',
-            choices: pickManager
-
-        },
-        {
-            type: 'list',
-            name: 'roleName',
-            message: 'Choose Employee Role',
-            choices: addRole
-        }
-        
-    ])
-    .then((ans) =>{
-
-        const roleID=addRole().indexOf(ans.roleName) + 1
-        const managerID=pickManager().indexOf(ans.managerName) + 1
-        
-        connection.query('INSERT INTO employee SET ?',{
-            First_Name: ans.firstName,
-            Last_Name: ans.lastName,
-            Role_ID: roleID,
-            Manager_ID: managerID
-        },
-        (err,res)=>{
-            if(err) throw err;
-            console.table(ans)
-            start();
-        })
-
-
     })
+
+    return roleArray;
 };
+
+const addEmployee = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'firstName',
+                message: 'Enter Empolyee First Name'
+            },
+            {
+                type: 'input',
+                name: 'lastName',
+                message: 'Enter Empolyee Last Name'
+            },
+            {
+                type: 'list',
+                name: 'managerName',
+                message: 'Enter Empolyee Manager',
+                choices: pickManager()
+
+            },
+            {
+                type: 'list',
+                name: 'roleName',
+                message: 'Choose Employee Role',
+                choices: addRole()
+            }
+
+        ])
+        .then((ans) => {
+
+            const roleID = addRole().indexOf(ans.roleName) + 1
+            const managerID = pickManager().indexOf(ans.managerName) + 1
+
+            connection.query('INSERT INTO employee SET ?', {
+                First_Name: ans.firstName,
+                Last_Name: ans.lastName,
+                Role_ID: roleID,
+                Manager_ID: managerID
+            },
+                (err, res) => {
+                    if (err) throw err;
+                    console.table(ans)
+                    start();
+                })
+
+
+        })
+};
+
+/*const removeEmployee = () =>{
+    //const query= connection.query('DELETE FROM employee WHERE Role_ID ?',
+    {
+        Role_ID: addRole().indexOf()
+    }
+    )
+} */
+
+const updateEmployee = () => {
+
+    connection.query('SELECT employee.Last_Name, roletype.Title FROM employee roletype WHERE employee.ID3=roleytpe.ID2', (err, res) => {
+        if (err) throw err;
+        console.log(res)
+
+
+        inquirer
+            .prompt([
+
+                {
+                    type: 'rawlist',
+                    name: 'newLastName',
+                    message: 'What is the New Employee Last Name?',
+                    choices: function () {
+                        const lastNameArr = [];
+                        for (let i = 0; i < res.length; i++) {
+                            lastNameArr.push(res[i].Last_Name)
+
+                        }
+
+                        return lastNameArr;
+
+                    }
+
+
+                },
+                {
+                    type: 'rawlist',
+                    name: 'newRole',
+                    message: 'Enter New Role',
+                    choices: addRole()
+                }
+            ])
+            .then((ans) => {
+                const roleIdUp = addRole().indexOf(ans.newRole) + 1
+                connection.query('UPDATE employee WHERE ?',
+                    {
+                        Last_Name: ans.newLastName
+                    },
+                    {
+                        Role_ID: roleIdUp
+                    },
+                    (err, res) => {
+                        if (err) throw err
+                        console.table(ans)
+                        start()
+                    })
+
+                });
+                
+                
+            })
+        };
+
+
+
+
+
+
+
